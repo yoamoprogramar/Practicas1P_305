696e85b03c512e5b3e2d500fc9a0021132529db6
Rene Dominguez <byskriom@gmail.com>
2022-08-25 10:06:00 -0500

    AGREGUE MI NOMBRE EN FIGURAS

--- src/practicas/Hola.java
+++ src/practicas/Hola.java
@@ -4,6 +4,7 @@ package practicas;
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola Mundo CRUEL");
+        System.out.println("RENE DOMINGUEZ ESCALONA");
         System.out.println("oooo  oooo  o  o  oooo");
         System.out.println("o  o  o     oo o  o   ");
         System.out.println("oooo  o     oo o  o");
